1c9eee245238cd21fa43489fed762981d6b97550
Jared <jared@needapart.co.nz>
2019-05-16 16:15:38 +1200

    Start working on subtraction

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,29 @@
-var temporaryAnswer = [];
+//define the 
+var temporaryAnswer = '';
+var number=[];
+var a = '';
+var b = '';
 
 function buttonPress(buttonValue){
     console.log(buttonValue)
     if (!isNaN(buttonValue)) {
-        console.log('is a number')
-        temporaryAnswer.push(buttonValue)
+        number.push(buttonValue)
+        
         //prints current value to the calculator screen
-        document.getElementById("answer").value=temporaryAnswer.join('');
-        console.log(temporaryAnswer);
-    }    
-    else if(buttonValue === '-' || buttonValue === '+' || buttonValue === '/' || buttonValue === '*') {
-        console.log('is not a number')
+        document.getElementById("answer").value=number.join('');
+        a = number.join('');
+        console.log(number);
+        console.log(a);
+    
+    }else if(buttonValue === '-' /*|| buttonValue === '+' || buttonValue === '/' || buttonValue === '*'*/) {
+        //clear number array and set a as temporary answer
+        number = [];
+        temporaryAnswer = a;
+        
+        //the subtract a from temporaryAnswer
+        answer = temporaryAnswer - a
+        console.log(answer)
+        printToCalc(answer)
     } else if(buttonValue === '.') {
         console.log('is a decimal point')
     } else if(buttonValue === '=') {
@@ -18,7 +31,10 @@ function buttonPress(buttonValue){
     }
 }
 
-
+//print to screen funcation
+function printToCalc(number){
+    document.getElementById("answer").value=number
+}
 
 
 
