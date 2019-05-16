506428e1a5128613b32d920aed7e2416727fc1dc
Jared <jared@needapart.co.nz>
2019-05-17 09:46:53 +1200

    pseudocode for calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,5 @@
-//define the 
+//define the calculator object
+
 var temporaryAnswer = '';
 var number=[];
 var a = '';
@@ -10,31 +11,34 @@ function buttonPress(buttonValue){
         number.push(buttonValue)
         
         //prints current value to the calculator screen
+        //calls the printCalc function first
         document.getElementById("answer").value=number.join('');
         a = number.join('');
         console.log(number);
         console.log(a);
-    
-    }else if(buttonValue === '-' /*|| buttonValue === '+' || buttonValue === '/' || buttonValue === '*'*/) {
-        //clear number array and set a as temporary answer
-        number = [];
-        temporaryAnswer = a;
-        
-        //the subtract a from temporaryAnswer
-        answer = temporaryAnswer - a
-        console.log(answer)
-        printToCalc(answer)
-    } else if(buttonValue === '.') {
-        console.log('is a decimal point')
-    } else if(buttonValue === '=') {
-        console.log('is an equal sign')
     }
-}
 
-//print to screen funcation
+//     else if(buttonValue === '-' /*|| buttonValue === '+' || buttonValue === '/' || buttonValue === '*'*/) {
+//         //clear number array and set a as temporary answer
+//         number = [];
+//         temporaryAnswer = a;
+        
+//         //the subtract a from temporaryAnswer
+//         answer = temporaryAnswer - a
+//         console.log(answer)
+//         printToCalc(answer)
+//     } else if(buttonValue === '.') {
+//         console.log('is a decimal point')
+//     } else if(buttonValue === '=') {
+//         console.log('is an equal sign')
+//     }
+// }
+
+//print to screen function - move to object
 function printToCalc(number){
     document.getElementById("answer").value=number
 }
+
 
 
 
