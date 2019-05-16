78b9e7607953d34eae912d321f616b48ceed18d3
Jared <jared@needapart.co.nz>
2019-05-16 15:27:53 +1200

    make screen receive numbers beyond 1 digit

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,24 @@
-function buttonPress(value){
-    console.log(value)
-    return value
+var temporaryAnswer = [];
+
+function buttonPress(buttonValue){
+    console.log(buttonValue)
+    if (!isNaN(buttonValue)) {
+        console.log('is a number')
+        temporaryAnswer.push(buttonValue)
+        //prints current value to the calculator screen
+        document.getElementById("answer").value=temporaryAnswer.join('');
+        console.log(temporaryAnswer);
+    }    
+    else if(buttonValue === '-' || buttonValue === '+' || buttonValue === '/' || buttonValue === '*') {
+        console.log('is not a number')
+    } else if(buttonValue === '.') {
+        console.log('is a decimal point')
+    } else if(buttonValue === '=') {
+        console.log('is an equal sign')
+    }
 }
-//document.addEventListener('click', () => {console.log('hpe')})
+
+
+
+
+
