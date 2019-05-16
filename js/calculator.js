54d722364bbee1e8441bc1bcdbf690040467d86e
Jared <jared@needapart.co.nz>
2019-05-17 11:12:05 +1200

    pseudocode rest of button press and make percent method

--- js/calculator.js
+++ js/calculator.js
@@ -8,12 +8,15 @@ var calculator = {
     number: '',
 
     /* Methods */
+    //calculation
     // come back and do something to control for string length, particularly with decimal points and large numbers
     add: (a,b) => { return a + b },
     sub: (a,b) => { return a - b},
     multiply: (a,b) => { return a * b},
     divide: (a,b) => { return a / b }, 
+    percent: (a) => {return a /100},
 
+    //display
     printToCalc: (number) => { document.getElementById("answer").value=number }
 };
 
@@ -24,6 +27,12 @@ function buttonPress(buttonValue) {
         calculator.number = calculator.numberArray.join('')
         calculator.printToCalc(calculator.number)
     }
+    // if .
+    // if % - dive answer by 100 and return value
+    // if AC - reintitilise calculator
+    // if CE - clear currrent (remove current number & operator)
+    // 
+    //if operator do something
         console.log(calculator)
 }
        
