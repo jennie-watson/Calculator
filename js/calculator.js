55d12ee4ae6d762eb44557cdbff9e379ff0ce818
Jared <jared@needapart.co.nz>
2019-05-17 13:38:30 +1200

    tidy up addition case and get to work

--- js/calculator.js
+++ js/calculator.js
@@ -5,7 +5,7 @@ var calculator = {
     finalAnswer: '',
     operator: '',
     numberArray: [],
-    number: '',
+    number: 0,
 
     /* Methods */
     //calculation
@@ -23,8 +23,11 @@ var calculator = {
 
 function buttonPress(buttonValue) {
     if (!isNaN(buttonValue)) {
+        //add buttonValue to an array
         calculator.numberArray.push(buttonValue)
-        calculator.number = calculator.numberArray.join('')
+        //join number array so that it gives multiple place values and convert from string to an integer
+        calculator.number = Number(calculator.numberArray.join(''))
+        //print to screen
         calculator.printToCalc(calculator.number) 
     } else{
         //check for stored operator
@@ -37,12 +40,17 @@ function buttonPress(buttonValue) {
             // store operator
             calculator.operator = buttonValue
         } else {
-       // stored operator
-                // check what the store operator is and use that function
-                    // set temp ans as opFunc(temp, num)
-            switch (calculator.operator) {
+            // check what the store operator is and use that function
+
+            // define local variables to use in the function
+            var operator = calculator.operator
+            var temporaryAnswer = calculator.temporaryAnswer
+            var number = calculator.number
+                
+            switch (operator) {
                 case '+':
-                    calculator.temporaryAnswer = calculator.add(calculator.temporaryAnswer, calculator.number) 
+                    calculator.temporaryAnswer = calculator.add(temporaryAnswer, number) 
+                    //console.log(temporaryAnswer + ' + ' + number + ' = ' + calculator.temporaryAnswer )
                     break;
                 default: 
                     console.log('not a + operator')
