736af7ed7982c8849096a81605a1830ecd1aaaeb
Jared <jared@needapart.co.nz>
2019-05-19 16:52:48 +1200

    make calculate function to tidy up lgic

--- js/calculator.js
+++ js/calculator.js
@@ -8,17 +8,14 @@ var calculator = {
     sub: (a,b) => { return a - b},
     multiply: (a,b) => { return a * b},
     divide: (a,b) => { return a / b }, 
-    percent: (a) => {return a / 100},
-    printToCalc: (num) => { document.getElementById("answer").value=num }
+    percent: (a) => {return a / 100}
 };
 
 
 function buttonPress(buttonValue) {
     if (!isNaN(buttonValue)) {
         // Handling buttonPress so the values display to the screen correctly
-        calculator.numberArray.push(buttonValue)
-        calculator.number = Number(calculator.numberArray.join(''))
-        calculator.printToCalc(calculator.number) 
+        printToCalc(buttonValue) 
     } else {
         if(!calculator.operator) {
             // Setting the first number as the current answer and clearing the way for the next arguement in the equation
@@ -28,44 +25,10 @@ function buttonPress(buttonValue) {
             // store operator
             calculator.operator = buttonValue
         } else {
-            // check what the store operator is and use that function
-
-            var operator = calculator.operator
-            var temporaryAnswer = calculator.temporaryAnswer
-            var number = calculator.number
+            calculate(calculator.temporaryAnswer,calculator.number,calculator.operator)
             
             // On the buttonPress of an operator it looks at the last portion of the equation and saves it to temporaryAnswer, then stores the new operator.
-            switch (operator) {
-                case '+':
-                    calculator.temporaryAnswer = calculator.add(temporaryAnswer, number) 
-                    // console.log(temporaryAnswer + ' + ' + number + ' = ' + calculator.temporaryAnswer )
-                    calculator.printToCalc(calculator.temporaryAnswer)
-                    break;
-                case '-':
-                    calculator.temporaryAnswer = calculator.sub(temporaryAnswer, number) 
-                    // console.log(temporaryAnswer + ' - ' + number + ' = ' + calculator.temporaryAnswer )
-                    calculator.printToCalc(calculator.temporaryAnswer)
-                    break;
-                case '*':
-                    calculator.temporaryAnswer = calculator.multiply(temporaryAnswer, number) 
-                    // console.log(temporaryAnswer + ' * ' + number + ' = ' + calculator.temporaryAnswer )
-                    calculator.printToCalc(calculator.temporaryAnswer)
-                    break;
-                case '/':
-                    calculator.temporaryAnswer = calculator.divide(temporaryAnswer, number) 
-                    // console.log(temporaryAnswer + ' / ' + number + ' = ' + calculator.temporaryAnswer )
-                    calculator.printToCalc(calculator.temporaryAnswer)
-                    break;
-                case '%':
-                    calculator.temporaryAnswer = calculator.percent(temporaryAnswer) 
-                    // console.log(temporaryAnswer + ' % ' + number + ' = ' + calculator.temporaryAnswer )
-                    calculator.printToCalc(calculator.temporaryAnswer)
-                    break;
-                default: 
-                    console.log(operator)
-                    console.log('not a known operator')
-            }
-
+        
             // clear the way for next arguement in the equation
             calculator.number = 0
             calculator.numberArray = []
@@ -73,4 +36,37 @@ function buttonPress(buttonValue) {
             calculator.operator = buttonValue
         }        
     }
+}
+
+function calculate(a,b,operator){
+    // check what the store operator is and use that function
+    switch (operator) {
+        case '+':
+            calculator.temporaryAnswer = calculator.add(a, b) 
+            // console.log(temporaryAnswer + ' + ' + number + ' = ' + calculator.temporaryAnswer )
+            break;
+        case '-':
+            calculator.temporaryAnswer = calculator.sub(a, b) 
+            // console.log(temporaryAnswer + ' - ' + number + ' = ' + calculator.temporaryAnswer )
+            break;
+        case '*':
+            calculator.temporaryAnswer = calculator.multiply(a, b) 
+            // console.log(temporaryAnswer + ' * ' + number + ' = ' + calculator.temporaryAnswer )
+            break;
+        case '/':
+            calculator.temporaryAnswer = calculator.divide(a, b) 
+            // console.log(temporaryAnswer + ' / ' + number + ' = ' + calculator.temporaryAnswer )
+            break;
+        case '%':
+            calculator.temporaryAnswer = calculator.percent(a) 
+            // console.log(temporaryAnswer + ' % ' + number + ' = ' + calculator.temporaryAnswer )
+            break;
+    }
+    printToCalc(calculator.temporaryAnswer)
+}
+
+function printToCalc(num) { 
+    calculator.numberArray.push(num)
+    calculator.number = Number(calculator.numberArray.join(''))
+    document.getElementById("answer").value=calculator.number
 }
